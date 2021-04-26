336ca9e7e8ba10ea1906467213a6265ef7bf681a
Adam Fortuna <adam@fortuna.name>
2021-04-25 22:50:07 -0600

    Removed auth0 and Apollo for now

--- pages/_app.js
+++ pages/_app.js
@@ -1,23 +1,13 @@
 import './globals.css'
-import App, { Container } from 'next/app'
-import React from 'react'
-import withApolloClient from '@lib/with-apollo-client'
-import { ApolloProvider } from '@apollo/client'
-import { UserProvider } from '@auth0/nextjs-auth0'
+import App from 'next/app'
 
 class MyApp extends App {
-render () {
-  const { Component, pageProps, apolloClient } = this.props
-  return (
-    <Container>
-      <ApolloProvider client={apolloClient}>
-        <UserProvider>
-          <Component {...pageProps} />
-        </UserProvider>
-      </ApolloProvider>
-    </Container>
-  )
-}
+  render () {
+    const { Component, pageProps } = this.props
+    return (
+      <Component {...pageProps} />
+    )
+  }
 }
 
-export default withApolloClient(MyApp)
+export default MyApp
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,8 @@
 import Head from 'next/head'
 import Layout from '@components/Layout'
-import { useUser } from '@auth0/nextjs-auth0';
 
 
 export default function Home() {
-  const { user, error, isLoading } = useUser();
-  let message
-
-  if (isLoading) {
-    message = <div>Loading...</div>
-  }
-  if (error) {
-    message = <div>{error.message}</div>
-  }
-  if (!user) {
-    message = <a href="/api/auth/login">Login</a>
-  }
-  else {
-    message = <a href="/api/auth/logout">Logout {user.name}</a>
-  }
 
   return (
     <Layout>
@@ -30,8 +14,6 @@ export default function Home() {
         <h1>
           This is an H1
         </h1>
-
-        {message}
       </main>
     </Layout>
   )

24b11f9ebde89438328a4383c03f9a6667f30166
Adam Fortuna <adam@fortuna.name>
2021-04-26 00:06:47 -0600

    Added new signin page

---
+++ pages/auth/signin.js
@@ -0,0 +1,127 @@
+import { getProviders, signIn } from 'next-auth/client'
+
+export default function SignIn({ providers }) {
+  return (
+    <div className="min-h-screen bg-white flex">
+      <div className="flex-1 flex flex-col justify-center py-12 px-4 sm:px-6 lg:flex-none lg:px-20 xl:px-24">
+        <div className="mx-auto w-full max-w-sm lg:w-96">
+          <div>
+            <img
+              className="h-12 w-auto"
+              src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
+              alt="Workflow"
+            />
+            <h2 className="mt-6 text-3xl font-extrabold text-gray-900">Jump Right In!</h2>
+          </div>
+
+          <div className="mt-8">
+            <div>
+              <div>              
+                <p className="text-sm font-medium text-gray-700">Sign up or sign in with...</p>
+
+                <div className="mt-2 grid grid-cols-3 gap-3">
+                  <div>                    
+                    <button onClick={() => signIn(providers.facebook.id)} className="w-full inline-flex justify-center py-2 px-4 border border-gray-300 rounded-md shadow-sm bg-white text-sm font-medium text-gray-500 hover:bg-gray-50">
+                      <span className="sr-only">Sign in with Facebook</span>
+                      <svg className="w-5 h-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20">
+                        <path
+                          fillRule="evenodd"
+                          d="M20 10c0-5.523-4.477-10-10-10S0 4.477 0 10c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V10h2.54V7.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V10h2.773l-.443 2.89h-2.33v6.988C16.343 19.128 20 14.991 20 10z"
+                          clipRule="evenodd"
+                        />
+                      </svg>
+                    </button>
+                  </div>
+
+                  <div>
+                    <button onClick={() => signIn(providers.google.id)} className="w-full inline-flex justify-center py-2 px-4 border border-gray-300 rounded-md shadow-sm bg-white text-sm font-medium text-gray-500 hover:bg-gray-50">
+                      <span className="sr-only">Sign in with Google</span>
+                      <svg className="w-5 h-5" aria-hidden="true" fill="currentColor" viewBox="0 0 488 512">
+                        <path fill="currentColor" d="M488 261.8C488 403.3 391.1 504 248 504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 123 24.5 166.3 64.9l-67.5 64.9C258.5 52.6 94.3 116.6 94.3 256c0 86.5 69.1 156.6 153.7 156.6 98.2 0 135-70.4 140.8-106.9H248v-85.3h236.1c2.3 12.7 3.9 24.9 3.9 41.4z"></path>
+                      </svg>
+                    </button>
+                  </div>
+
+                  <div>
+                    <button onClick={() => signIn(providers.apple.id)} className="w-full inline-flex justify-center py-2 px-4 border border-gray-300 rounded-md shadow-sm bg-white text-sm font-medium text-gray-500 hover:bg-gray-50">
+                      <span className="sr-only">Sign in with Apple</span>
+                      <svg className="w-5 h-5" aria-hidden="true" fill="currentColor"  viewBox="0 0 384 512">
+                        <path fill="currentColor" d="M318.7 268.7c-.2-36.7 16.4-64.4 50-84.8-18.8-26.9-47.2-41.7-84.7-44.6-35.5-2.8-74.3 20.7-88.5 20.7-15 0-49.4-19.7-76.4-19.7C63.3 141.2 4 184.8 4 273.5q0 39.3 14.4 81.2c12.8 36.7 59 126.7 107.2 125.2 25.2-.6 43-17.9 75.8-17.9 31.8 0 48.3 17.9 76.4 17.9 48.6-.7 90.4-82.5 102.6-119.3-65.2-30.7-61.7-90-61.7-91.9zm-56.6-164.2c27.3-32.4 24.8-61.9 24-72.5-24.1 1.4-52 16.4-67.9 34.9-17.5 19.8-27.8 44.3-25.6 71.9 26.1 2 49.9-11.4 69.5-34.3z"></path>
+                      </svg>
+                    </button>
+                  </div>
+                </div>
+              </div>
+
+              <div className="mt-6 relative">
+                <div className="absolute inset-0 flex items-center" aria-hidden="true">
+                  <div className="w-full border-t border-gray-300" />
+                </div>
+                <div className="relative flex justify-center text-sm">
+                  <span className="px-2 bg-white text-gray-500">Or continue with</span>
+                </div>
+              </div>
+            </div>
+
+            <div className="mt-6">
+              <form action="#" method="POST" className="space-y-6">
+                <div>
+                  <h3 className="text-lg text-gray-700 max-w font-semibold">No Passwords Here</h3>
+                  <p className="text-sm text-gray-600 max-w">
+                    Enter your email address to sign up for a new account, or access your existing one.
+                  </p>
+                </div>
+
+
+                <div>
+                  <label htmlFor="email" className="block text-sm font-medium text-gray-700">
+                    Email address
+                  </label>
+                  <div className="mt-1">
+                    <input
+                      id="email"
+                      name="email"
+                      type="email"
+                      autoComplete="email"
+                      required
+                      className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                    />
+                  </div>
+                </div>
+
+                <div>
+                  <button
+                    type="submit"
+                    className="w-full flex items-center justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                  >
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 transform rotate-45 -mt-1 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                      <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 19l9 2-9-18-9 18 9-2zm0 0v-8" />
+                    </svg>
+
+                    Send Magic Link
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="hidden lg:block relative w-0 flex-1">
+        <img
+          className="absolute inset-0 h-full w-full object-cover"
+          src="https://images.unsplash.com/photo-1505904267569-f02eaeb45a4c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1908&q=80"
+          alt=""
+        />
+      </div>
+    </div>
+  )
+}
+
+
+// This is the recommended way for Next.js 9.3 or newer
+export async function getServerSideProps(context){
+  const providers = await getProviders()
+  return {
+    props: { providers }
+  }
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,24 @@
 import Head from 'next/head'
 import Layout from '@components/Layout'
+import { useSession, signIn, signOut } from 'next-auth/client'
 
 
 export default function Home() {
+  const [ session, loading ] = useSession()
+  let message
+  if(session) {
+    message = <>
+      Signed in as {session.user?.email} <br/>
+      <button onClick={() => signOut()}>Sign out</button>
+    </>
+  } else {
+    message = <>
+      <p>
+        Not signed in <br/>
+        <button onClick={() => signIn()}>Sign in</button>
+      </p>
+    </>
+  }
 
   return (
     <Layout>
@@ -14,6 +30,10 @@ export default function Home() {
         <h1>
           This is an H1
         </h1>
+
+        <p>
+          {message}
+        </p>
       </main>
     </Layout>
   )

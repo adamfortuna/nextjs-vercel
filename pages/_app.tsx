b522b0122fca3397f46aa7d2d39547a44c509981
Adam Fortuna <adam@fortuna.name>
2021-04-27 16:10:59 -0600

    Graphql working with a hardcoded token

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,20 @@
 import './globals.css'
 import App from 'next/app'
 import { Provider } from 'next-auth/client'
+import { ApolloProvider } from '@apollo/client';
+import { useApollo } from "@lib/apollo"
 
 class MyApp extends App {
-
   render () {
     const { Component, pageProps } = this.props
+
+    const apolloClient = useApollo(pageProps.initialApolloState)
+
     return (
       <Provider session={pageProps.session}>
-        <Component {...pageProps} />
+        <ApolloProvider client={apolloClient}>
+          <Component {...pageProps} />
+        </ApolloProvider>
       </Provider>
     )
   }
---
+++ pages/books/index.tsx
@@ -0,0 +1,69 @@
+import { gql, useQuery } from '@apollo/client';
+import Head from 'next/head'
+import Layout from '@components/Layout'
+
+const query = gql`
+query GetBooks {
+  books {
+    id
+    title
+  }
+}
+`
+
+function Books() {
+
+  const { data, loading, error } = useQuery(query);
+  
+  if (loading) {
+    return (
+      <Layout>
+        <h2>Bypass...</h2>
+      </Layout>
+    )
+  }
+
+  if (error) {
+    return (
+      <Layout>
+        <h2>Error...</h2>
+        <p>{error}</p>
+      </Layout>
+    )
+  }
+
+  console.log('data', data)
+
+  return (
+    <Layout>
+      <h2>LOADED...</h2>
+    </Layout>
+  )
+
+
+  // const books = data.books || []
+
+  // return (
+  //   <Layout>
+  //     <Head>
+  //       <title>Books</title>
+  //     </Head>
+
+  //     <main className="container mx-auto">
+  //       <h1>
+  //         Books
+  //       </h1>
+
+  //       <p>This is the books page with {books.length} books.</p>
+
+  //       {books.map((books) => (
+  //         <div key={book.id}>
+  //           <h3>{book.title}</h3>
+  //        </div>
+  //       ))}
+  //     </main>
+  //   </Layout>
+  // )
+}
+
+export default Books

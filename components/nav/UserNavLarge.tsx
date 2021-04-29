022982a0e31398793058c1acfc8d4e3fca23109b
Adam Fortuna <adam@fortuna.name>
2021-04-29 11:34:56 -0600

    Dark mode to nav icons

--- components/nav/UserNavLarge.tsx
+++ components/nav/UserNavLarge.tsx
@@ -15,7 +15,7 @@ export default function UserNavLarge({ user }:any) {
   return (
     <div className="hidden lg:block lg:ml-4">
       <div className="flex items-center">
-        <button className="flex-shrink-0 bg-gray-800 p-1 rounded-full text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
+        <button className="flex-shrink-0 dark:bg-gray-800 p-1 rounded-full text-gray-400 hover:text-gray-800 dark:hover:bg-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
           <span className="sr-only">View notifications</span>
           <BellIcon className="h-6 w-6" aria-hidden="true" />
         </button>
